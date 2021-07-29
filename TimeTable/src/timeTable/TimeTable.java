c11bb9d6a8f9d1298f7d8ca1921f7e38e8063992
andrio <Andrio1hanania@gmail.com>
2021-07-29 16:29:15 +0300

    Merge branch 'master' of https://github.com/AstroMan7/EvolutionaryTimeTable   4

--- TimeTable/src/timeTable/TimeTable.java
+++ TimeTable/src/timeTable/TimeTable.java
@@ -14,6 +14,7 @@ import java.util.Random;
 
 public class TimeTable extends Solution implements Problem<TimeTable>
 {
+    //htrsfhd
     //Members
     private List<TimeTableChromosome> m_Chromosomes;
     private List<Teacher> m_Teachers;
